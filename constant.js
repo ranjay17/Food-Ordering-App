286489f99bff1704c06e7a274452af084953bf38
Ranjay Singh <Sranjay15@gmail.com>
2024-06-23 13:38:34 +0530

    constant.js

---
+++ constant.js
@@ -0,0 +1,7 @@
+
+//named export
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
+
+export const LOGO_URL = "https://webstockreview.net/images/clipart-food-simple-14.png"
+
+export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.2513844&lng=81.62964130000002&restaurantId="
